dd039f1a8c7ea6f758503a9af2146df9f840f950
Ankit Jha <vayugoankit@gmail.com>
2023-04-08 10:56:53 +0000

    getting into effciency

--- commonOccurenceArrays.js
+++ commonOccurenceArrays.js
@@ -1,5 +1,9 @@
 //Create a function with two arrays and return True or False if the arrays share a common element
 
+// Ask what is important time complexity or space complexity
+
+//About array whether it is static or scalable with time, whether it has same type of elements or not
+
 function arrayVal(arr1, arr2){
   for(let i=0; i<arr1.length; i++){
     for(let j=0; j<arr2.length; j++){
@@ -11,3 +15,5 @@ function arrayVal(arr1, arr2){
   return false; 
 }
 arrayVal(["a","b",'c','d'], ["e","f","g",'h'])
+
+//Not very efficient, O(n^2)
